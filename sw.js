cc44fb786001a0e8deec492a83e5c904fef6171d
tolkinjonofficel-del <tolkinjonofficel@gmail.com>
2026-05-23 10:12:25 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,21 @@
-// sw.js
-const CACHE_NAME = '1xbet-signals-v1';
+const CACHE_NAME = 'saytapp-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/manifest.json'
+  '/assets/css/style.css',
+  '/assets/js/main.js'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
+      .then(response => response || fetch(event.request))
   );
 });
